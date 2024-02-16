6415ff0fba4e9159cba4b42f9c495384b521f40c
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2024-02-16 13:58:26 +0100

    Update data.js again

--- src/components/data.js
+++ src/components/data.js
@@ -28,7 +28,52 @@ const data = [
     "id": 6,
     "title": "IDK",
     "description": "I dont know"
-    }
+    },
+    {
+    "id": 7,
+    "title": "BRB",
+    "description": "Be right back"
+  },
+  {
+    "id": 8,
+    "title": "TTYL",
+    "description": "Talk to you later"
+  },
+  {
+    "id": 9,
+    "title": "FYI",
+    "description": "For your information"
+  },
+  {
+    "id": 10,
+    "title": "IMO",
+    "description": "In my opinion"
+  },
+  {
+    "id": 11,
+    "title": "IRL",
+    "description": "In real life"
+  },
+  {
+    "id": 12,
+    "title": "GTG",
+    "description": "Got to go"
+  },
+  {
+    "id": 13,
+    "title": "AFK",
+    "description": "Away from keyboard"
+  },
+  {
+    "id": 14,
+    "title": "BFF",
+    "description": "Best friends forever"
+  },
+  {
+    "id": 15,
+    "title": "NSFW",
+    "description": "Not safe for work"
+  }
 ]
 
 export default data
